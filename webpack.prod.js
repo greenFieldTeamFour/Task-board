1a66c6ee65c67f38c5daa1e4791463b6df76a579
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-04-16 14:18:39 -0500

    "added Bruno's boilerplate"

---
+++ webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['./client/src/Index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules|packages/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.NamedModulesPlugin(),
+  ],
+};
