14a957eae3012cb81b07b133bd1579c771647936
Mike <diazdeleonjhon@gmail.com>
2019-04-18 11:03:12 -0500

    Commented everything for better understanding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        {/* Render AddTask */}
         <AddTask />
       </div>
     )
--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -4,24 +4,30 @@ export default class AddTask extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			//Create a placeholder for the user to enter their input
 			userInput: '',
+			//place holder to save user input
 			list: []
 		}
 	}
-
+	//handle user input
 	changeUserInput(input) {
 		this.setState({
 			userInput: input
 		});
 	}
-
+	
 	addTask(input){
+		//placeholder for what's being added
 		let taskArray = this.state.list;
+		// edge case for the user not to be able to enter an empty task
 		if (input === ''){
 			alert("Please enter a task!")
 		} else {
-		taskArray.push(input);
+			//  push the input into placeholder
+			taskArray.push(input);
 		}
+		//reset the input box once sumbit is clicked on
 		this.setState({
 			list: taskArray,
 			userInput: ''
@@ -36,14 +42,18 @@ export default class AddTask extends Component {
 					<div>
 						<input
 							placeholder = "Enter a new task"
+							// handle the user input 
 							onChange={ (e)=>this.changeUserInput(e.target.value)}
+							// set value to the userInput from state
 							value={this.state.userInput}
 							type="text"
-							/>
-							<button onClick={()=> this.addTask(this.state.userInput)}>Add Task</button>
-							<ul>
-								{this.state.list.map( (val)=> <li>{val}</li>)}
-							</ul>
+						/>
+						{/* onClick so when the submit button is clicked the input and be saved */}
+						<button onClick={()=> this.addTask(this.state.userInput)}>Add Task</button>
+						<ul>
+							{/*iterate through list and return it so its displayed*/}
+							{this.state.list.map( (val)=> <li>{val}</li>)}
+						</ul>
 					</div>
 				</center>
 			</div>
