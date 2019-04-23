4cdc2a3f6c5787eeb17af4b4f8a2ddef8303f220
LuisAguilarPena <luis.aguilar.pena123@gmail.com>
2019-04-23 16:51:31 -0500

    added simplistic style and installed react transition in case we need it

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import { CSSTransition } from 'react-transition-group';
 
 export default class AddTask extends Component {
 	constructor(props) {
@@ -72,8 +73,8 @@ export default class AddTask extends Component {
 		return (
 			<div>
 				<div>
-					<h1>Task Board</h1>
-						<h4>Task Description:
+					<h1>Kaizen</h1>
+						<label>Description: </label>
 						<input
 							placeholder="Enter a new task"
 							// handle the user input
@@ -92,15 +93,15 @@ export default class AddTask extends Component {
 						<button onClick={() => {
 							console.log('task submitted');
 							this.addTask(this.state.userInput);
-						}}>Add Task</button>
-					</h4>
+						}}>Add</button>
+					
 				</div>
 				<br />
 				<br />
 				<div>
 					{/*iterate through list and return it so its displayed*/}
 					{this.state.list.map((val, index) => {
-						return (<div key={index}><li>{val.task} <button onClick={() => { this.deleteTask(val.task) }}>Done</button></li></div>
+						return (<div key={index}><p>{val.task} <button className="done" onClick={() => { this.deleteTask(val.task) }}>Done</button></p></div>
 						)
 					})}
 				</div>
