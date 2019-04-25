110e7a433d4007b80f3d15d962da03be36247307
Mike <diazdeleonjhon@gmail.com>
2019-04-25 12:13:54 -0500

    User input is now draggable, some CSS changes

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -20,6 +20,35 @@ export default class AddTask extends Component {
 			userInput: input
 		});
 	}
+
+	onDragStart = (e, index) => {
+		this.draggedItem = this.state.list[index];
+		e.dataTransfer.effectAllowed = "move";
+		e.dataTransfer.setData("text/html", e.target.parentNode);
+		e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+	};
+
+	onDragOver = index => {
+		const draggedOverItem = this.state.list[index];
+
+		// if the item is dragged over itself, ignore
+		if (this.draggedItem === draggedOverItem) {
+			return;
+		}
+
+		// filter out the currently dragged item
+		let list = this.state.list.filter(item => item !== this.draggedItem);
+
+		// add the dragged item after the dragged over item
+		list.splice(index, 0, this.draggedItem);
+
+		this.setState({ list });
+	};
+
+	onDragEnd = () => {
+		this.draggedIdx = null
+	};
+
 	//post method to send data to the server wich will be than transfered to the database
 	addTask(task){
 		console.log(`New task added: ${task}`);
@@ -102,13 +131,21 @@ export default class AddTask extends Component {
 					{/*iterate through list and return it so its displayed*/}
 					{this.state.list.map((val, index) => {
 						return (
-							<div key={index}>
-								<p>{val.task}
-									<button className="done" onClick={() => { this.deleteTask(val.task) }}>
-										Mastered!
-									</button>
-								</p>
-								<ProgressMeter/>
+							<div className = "App">
+								<p key={index} onDragOver= {() => this.onDragOver(index)} >
+									<div
+										className="drag"
+										draggable
+										onDragStart= {e => this.onDragStart(e, index)}
+										onDragEnd = {this.onDragEnd}
+									>
+										{val.task}
+											<button className="done" onClick={() => { this.deleteTask(val.task) }}>
+												Mastered!
+											</button>
+										<ProgressMeter/>
+									</div>
+										</p>
 							</div>
 						)
 					})}
